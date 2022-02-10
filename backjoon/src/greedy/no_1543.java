ed484b0cebe6073c4359b97b23e47500e114eef7
monsileI <https://github.com/MonsileI/pythonDjango.git>
2022-02-10 16:23:07 +0900

    to

--- backjoon/src/greedy/no_1543.java
+++ backjoon/src/greedy/no_1543.java
@@ -1,35 +1,37 @@
+
+
 package greedy;
+
+
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class test {
 
-	public static void main(String[] args) throws IOException {
+public class no_1543 {
+
+	public static void main(String[] args) throws IOException  {
 		
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
 	String doc = br.readLine();
-	String word = br.readLine();
-	int count = 0;
+	String search = br.readLine();
 	
-	while(doc.length()>0){
-		
-		if(doc.startsWith(word)) {
-			
-			doc = doc.substring(word.length());
-			count++;
+	int cnt=0;
+	while(doc.length()>0) {
+		if(doc.startsWith(search)) {
+			cnt++;
+			doc = doc.substring(search.length());
 		}else {
-			
 			doc = doc.substring(1);
-			
 		}
 		
-		
 	}
-	System.out.println(count);
 	
+	System.out.println(cnt);
 	}
 }
+
